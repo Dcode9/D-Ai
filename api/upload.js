1da6533727e5262dfc30669eb80ecaf20bdba36f
Dhairya Shah <shah.dhairy@outlook.com>
2026-05-27 15:21:46 +0000

    Skip Blob upload when storage is unconfigured

--- api/upload.js
+++ api/upload.js
@@ -40,11 +40,17 @@ function makeUploadRequest(req, body) {
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
+  }
+
+  if (req.method === 'GET') {
+    return sendJson(res, {
+      available: !!process.env.BLOB_READ_WRITE_TOKEN
+    });
   }
 
   if (req.method !== 'POST') {
